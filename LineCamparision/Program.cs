1e27feb764163222491ce0285e2efd1051d4d81e
yashdhanvijay <yash10dhanvijay.com>
2022-11-09 21:20:02 +0530

    [YASH]Add: Folder Sturcture Created

---
+++ LineCamparision/Program.cs
@@ -0,0 +1,16 @@
+﻿using LineComparisionProblem;
+using System;
+
+namespace LineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparision problem statement ");
+            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
+            calculate.Calculate();
+
+        }
+    }
+}
